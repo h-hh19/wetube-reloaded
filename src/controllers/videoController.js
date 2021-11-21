d9e8048869ff08107a403d581dcd7ca281f64320
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-11-21 23:36:21 +0900

    6.3 nomadcoder
    6.3까지 학습함.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -28,29 +28,24 @@ let videos = [
 export const trending = (req,res) =>{
     
     return res.render("home",{pageTitle:"Home", videos});
-}
+};
 
 export const watch = (req, res) =>{
     const { id } = req.params;
     const video = videos[id-1]
     return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
-}
+};
 
-export const edit = (req, res) => {
-    res.render("edit");
-}
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1]
+    return res.render("edit", {pageTitle:`Editing: ${video.title}`,video});
+};
 
-export const search = (req, res) =>{
-    res.send("Search");
-}
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id-1].title = title;
 
-export const upload = (req, res) =>{
-    res.send("Upload");
-}
-
-export const deleteVideo = (req, res) =>{
-    console.log(req.params);
-    return res.send("Delet Video");
-}
-
-
+    return res.redirect(`/videos/${id}`);
+} ;
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -9,6 +9,5 @@ const globalRouter = express();
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login); 
-globalRouter.get("/search", search);
 
 export default globalRouter;
